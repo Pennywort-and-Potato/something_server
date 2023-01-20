1f214d8ed31e6b9968e393301d350d7797d62302
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-01-20 12:56:21 +0700

    SEED
    Seed dummy auth data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'bcrypt'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(username: "realkhoa", password: BCrypt::Password.create("test"), name: "NGUYEN BA KHOA", email: "a@b", role: "admin")
+user2 = User.create(username: "fakeduy", password: BCrypt::Password.create("test2"), name: "DUY", email: "a@b", role: "admin")
