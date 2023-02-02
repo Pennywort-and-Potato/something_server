1d56d5c1ac79c5eb2efe57e4ef3c7b5d8c29ca92
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-02-03 00:49:56 +0700

    Done Register
    Validate username (no whitespace, spec char)
    Validate email
    Register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true, uniqueness: true 
-  validates :email, presence: true, uniqueness: true 
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, presence: true
 end
