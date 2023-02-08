88ac45782ec9fbfbc75a99eadfbd6b47a70460f2
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-02-08 17:59:01 +0700

    Update content.rb
    Remove the uniq constrain on Content src

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :post
 
-  validates :src, presence: true, uniqueness: true
+  validates :src, presence: true
   validates :content_type, presence: true
 end
