c23e43d08f398fa7ea9bc2943d4954159ca404a0
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-02-21 19:22:37 +0700

    Skip token check

--- app/controllers/v2/streaming/image_controller.rb
+++ app/controllers/v2/streaming/image_controller.rb
@@ -1,6 +1,6 @@
 class V2::Streaming::ImageController < ApplicationController
+  skip_before_action :authenticate_request
   def send_image
-
     file = Rails.root.join('public', 'images', "#{params[:file_name]}.png")
 
     if (!File.exist? file)
